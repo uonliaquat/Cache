9073ade0528ab81f1207f947fd68c384090513cb
Uon Liaquat <antelopes06@gmail.com>
2022-02-26 20:02:27 +0500

    Added CMakeLists.txt and main.cpp

---
+++ build/CMakeFiles/cache.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cache.dir/main.cpp.o"
+  "CMakeFiles/cache.dir/main.cpp.o.d"
+  "cache"
+  "cache.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cache.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
